122f9eb63d6ab7b2548afa70c3f92daf97b4b01f
hudengjun <hudengjun@corp.netease.com>
2022-05-23 19:18:31 +0800

    add for vcpkg

--- aop/mcaspect.hpp
+++ aop/mcaspect.hpp
@@ -26,56 +26,51 @@ SOFTWARE.
 #ifndef MC_ASPECT_HPP
 #define MC_ASPECT_HPP
 
+#include <aop/noncopyable.hpp>
 #include <concepts>
 #include <utility>
-#include "../noncopyable.hpp"
 
+namespace mc {
 
-namespace mc
-{
-
-template<typename Advice, typename... Args>
-concept HasMemberBefore = requires (Advice advice) { advice.before(std::declval<Args>()...); };
-
-template<typename Advice, typename... Args>
-concept HasMemberAfter = requires (Advice advice) { advice.after(std::declval<Args>()...); };
-
-template<typename Func, typename... Args>
-struct aspect final : noncopyable
-{
-	aspect(Func&& func) : func_(std::forward<Func>(func))
-	{}
-
-	template<typename... Advice>
-	void invoke(Args&&... args, Advice&&... advice) const
-	{
-		auto invoke_before = [&](auto&& ad) {
-			if constexpr (HasMemberBefore<decltype(ad), Args...>) 
-				ad.before(std::forward<Args>(args)...);
-		};
-
-		auto invoke_after = [&](auto&& ad) {
-			if constexpr (HasMemberAfter<decltype(ad), Args...>) 
-				ad.after(std::forward<Args>(args)...);
-		};
-
-		int dummy;
-		(invoke_before(advice), ...);
-		func_(std::forward<Args>(args)...);
-		(dummy = ... = (invoke_after(advice), 0));
-	}
-
-private:
-	Func func_;
+template <typename Advice, typename... Args>
+concept HasMemberBefore = requires(Advice advice) {
+  advice.before(std::declval<Args>()...);
 };
 
+template <typename Advice, typename... Args>
+concept HasMemberAfter = requires(Advice advice) {
+  advice.after(std::declval<Args>()...);
+};
 
+template <typename Func, typename... Args> struct aspect final : noncopyable {
+  aspect(Func &&func) : func_(std::forward<Func>(func)) {}
 
-template<typename... Advice, typename... Args, typename Func>
-void make_aspect(Func&& func, Args&&... args)
-{
-	mc::aspect<Func, Args...> ap(std::forward<Func>(func));
-	ap.invoke(std::forward<Args>(args)..., Advice()...);
+  template <typename... Advice>
+  void invoke(Args &&...args, Advice &&...advice) const {
+    auto invoke_before = [&](auto &&ad) {
+      if constexpr (HasMemberBefore<decltype(ad), Args...>)
+        ad.before(std::forward<Args>(args)...);
+    };
+
+    auto invoke_after = [&](auto &&ad) {
+      if constexpr (HasMemberAfter<decltype(ad), Args...>)
+        ad.after(std::forward<Args>(args)...);
+    };
+
+    int dummy;
+    (invoke_before(advice), ...);
+    func_(std::forward<Args>(args)...);
+    (dummy = ... = (invoke_after(advice), 0));
+  }
+
+private:
+  Func func_;
+};
+
+template <typename... Advice, typename... Args, typename Func>
+void make_aspect(Func &&func, Args &&...args) {
+  mc::aspect<Func, Args...> ap(std::forward<Func>(func));
+  ap.invoke(std::forward<Args>(args)..., Advice()...);
 }
 
 } // namespace mc
